adb49550f668c4c225d9e9fa2eb60b3d1751775d
the-real-girum <girum@girum.org>
2012-06-07 17:32:15 -0700

    Formatting test.

--- Android/src/edu/berkeley/eecs/ruzenafit/Tracking.java
+++ Android/src/edu/berkeley/eecs/ruzenafit/Tracking.java
@@ -9,16 +9,14 @@ public class Tracking extends Activity {
 	// Initializing variables
 	Button buttonRanking, buttonTracking, buttonPreferences;
 
-	
 	private static String t = "Message";
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.tracking);
 
-                
-				Log.d(t, "Tracking Screen !!!!");
-}
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tracking);
+
+		Log.d(t, "Tracking Screen !!!!");
+	}
 }
