52e5dba313f42037ec788c67cb50c3315cee8705
the-real-girum <girum@girum.org>
2012-06-15 15:49:27 -0700

    Can now pass sample data from GAE to Android.

--- Android/src/edu/berkeley/eecs/ruzenafit/access/GAEConnection.java
+++ Android/src/edu/berkeley/eecs/ruzenafit/access/GAEConnection.java
@@ -8,8 +8,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import android.util.Log;
 import edu.berkeley.eecs.ruzenafit.model.AnActualWorkoutModelX_X;
@@ -26,7 +27,7 @@ public class GAEConnection {
 	
 //	/** The username for the GAE account */
 //	private static final String username = "ibssagirum";
-	private static final String URL = "http://ruzenafit.appspot.com/rest/hello";
+	private static final String URL = "http://ruzenafit.appspot.com/rest/test";
 //	private static final String URL = "http://localhost:8888/rest/hello";
 	private static String deviceID = "";
 	
@@ -58,14 +59,30 @@ public class GAEConnection {
 			HttpResponse response = httpClient.execute(request);
 			HttpEntity entity = response.getEntity();
 			
-			// Spit out the string literal of the XML
-//			Log.d(TAG, EntityUtils.toString(entity));
-			
 			// Parse the resulting XML
 			DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 			Document result = documentBuilder.parse(entity.getContent());
 			
-			Log.d(TAG, "Hello world string: " + result.getDocumentElement().getFirstChild().getNodeValue());
+//			Log.d(TAG, "Hello world string: " + result.getDocumentElement().getFirstChild().getNodeValue());
+			NodeList workouts = result.getDocumentElement().getChildNodes();
+			
+			// For each of the workouts...
+			for (int i = 0; i < workouts.getLength(); i++) {
+				
+				NodeList workout = workouts.item(i).getChildNodes();
+				Log.d(TAG, "WORKOUT: ");
+				
+				// For each of this workout's XML children...
+				for (int j = 0; j < workout.getLength(); j++) {
+					Node workoutElement = workout.item(j);
+					
+					Log.d(TAG, "field " + workoutElement.getNodeName() + ": " +
+							workoutElement.getFirstChild().getNodeValue());
+				}
+				
+			}
+			
+//			Log.d(TAG, "Workouts: " + )
 			
 		}
 		catch (Exception e) {
