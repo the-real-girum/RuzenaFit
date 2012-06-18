83ed9e48c2bdfa08b2e392e322e6137753b77f34
the-real-girum <girum@girum.org>
2012-06-17 20:17:23 -0700

    Changed the URL.

--- Android/src/edu/berkeley/eecs/ruzenafit/access/GAEConnection.java
+++ Android/src/edu/berkeley/eecs/ruzenafit/access/GAEConnection.java
@@ -1,13 +1,21 @@
 package edu.berkeley.eecs.ruzenafit.access;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -27,7 +35,7 @@ public class GAEConnection {
 	
 //	/** The username for the GAE account */
 //	private static final String username = "ibssagirum";
-	private static final String URL = "http://ruzenafit.appspot.com/rest/test";
+	private static final String URL = "http://ruzenafit.appspot.com/rest/workout/test";
 //	private static final String URL = "http://localhost:8888/rest/hello";
 	private static String deviceID = "";
 	
@@ -37,6 +45,39 @@ public class GAEConnection {
 	 */
 	public static boolean submitDataToGAE(AnActualWorkoutModelX_X[] allWorkouts) {
 		Log.d(TAG, "submitDataToGAE: " + allWorkouts.toString());
+		
+		// Throw all of the fields of the workout into a list of nameValuePairs for
+		// the POST request to use.
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		
+//		AnActualWorkoutModelX_X workout = allWorkouts[0];
+		
+		// FIXME: I HATE HACKSSSSS.  String concatenation of array index here
+		for (int i = 0; i < allWorkouts.length; i++) {
+			nameValuePairs.add(new BasicNameValuePair("date" + "["+i+"]", 			allWorkouts[i].getDate()));
+			nameValuePairs.add(new BasicNameValuePair("duration" + "["+i+"]", 		allWorkouts[i].getDuration()));
+			nameValuePairs.add(new BasicNameValuePair("averageSpeed" + "["+i+"]", 	allWorkouts[i].getAverageSpeed()));
+			nameValuePairs.add(new BasicNameValuePair("totalCalories" + "["+i+"]", 	allWorkouts[i].getTotalCalories()));
+			nameValuePairs.add(new BasicNameValuePair("totalDistance" + "["+i+"]", 	allWorkouts[i].getTotalDistance()));
+		}
+		
+		
+		// Setup the POST Request
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpPost request = new HttpPost("http://ruzenafit.appspot.com/rest/workout/saveWorkout");
+		request.addHeader("deviceID:" , deviceID);
+		
+		// Execute the POST request.
+		try {
+			request.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			
+			HttpResponse response = httpClient.execute(request);
+			Log.d(TAG, "HttpResponse: " + EntityUtils.toString(response.getEntity()));
+			
+		} catch (Exception e) {
+			Log.e(TAG, "ERROR: " + e.getMessage());
+			return false;
+		}
 		
 		return true;
 	}
@@ -63,14 +104,15 @@ public class GAEConnection {
 			DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 			Document result = documentBuilder.parse(entity.getContent());
 			
-//			Log.d(TAG, "Hello world string: " + result.getDocumentElement().getFirstChild().getNodeValue());
 			NodeList workouts = result.getDocumentElement().getChildNodes();
+			allWorkouts = new AnActualWorkoutModelX_X[workouts.getLength()];
 			
 			// For each of the workouts...
 			for (int i = 0; i < workouts.getLength(); i++) {
 				
 				NodeList workout = workouts.item(i).getChildNodes();
 				Log.d(TAG, "WORKOUT: ");
+				allWorkouts[i] = new AnActualWorkoutModelX_X();
 				
 				// For each of this workout's XML children...
 				for (int j = 0; j < workout.getLength(); j++) {
@@ -78,12 +120,23 @@ public class GAEConnection {
 					
 					Log.d(TAG, "field " + workoutElement.getNodeName() + ": " +
 							workoutElement.getFirstChild().getNodeValue());
+					if (workoutElement.getNodeName().equals("averageSpeed")) {
+						allWorkouts[i].setAverageSpeed(workoutElement.getFirstChild().getNodeValue());
+					}
+					else if (workoutElement.getNodeName().equals("date")) {
+						allWorkouts[i].setDate(workoutElement.getFirstChild().getNodeValue());
+					}
+					else if (workoutElement.getNodeName().equals("duration")) {
+						allWorkouts[i].setDuration(workoutElement.getFirstChild().getNodeValue());
+					}
+					else if (workoutElement.getNodeName().equals("totalCalories")) {
+						allWorkouts[i].setTotalCalories(workoutElement.getFirstChild().getNodeValue());
+					}
+					else if (workoutElement.getNodeName().equals("totalDistance")) {
+						allWorkouts[i].setTotalDistance(workoutElement.getFirstChild().getNodeValue());
+					}
 				}
-				
 			}
-			
-//			Log.d(TAG, "Workouts: " + )
-			
 		}
 		catch (Exception e) {
 			Log.e(TAG, "Exception occurred: " + e.getMessage());
