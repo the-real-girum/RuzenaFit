5dac3f72c9f9d97c92c5fff0c5af8769c92e085b
Maurice Grant <mgrant1@ithaca.edu>
2012-06-18 17:02:32 -0700

    twit 2

--- Android/src/edu/berkeley/eecs/ruzenafit/Twit.java
+++ Android/src/edu/berkeley/eecs/ruzenafit/Twit.java
@@ -1,6 +1,7 @@
 package edu.berkeley.eecs.ruzenafit;
 
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,18 +19,28 @@ public class Twit extends Activity {
 
 		setContentView(R.layout.twitter);
 
-		//editStatus = (EditText) findViewById(R.id.edit_status);
+		// editStatus = (EditText) findViewById(R.id.edit_status);
 
 	}
 
 	public void onClick(View v) {
-		String statusText = editStatus.getText().toString();
+		final String statusText = editStatus.getText().toString();
+		// statusText = "The user burned " + "insert variable for calories here"
+		// + "calories";
 
-		Twitter twitter = new Twitter("student", "password");
-		
-		//twitter.setAPIRootUrl("http://yamba.markana.com/api");
-		twitter.setStatus(statusText);
-		
+		new Thread() {
+			public void run() {
+				try {
+					Twitter twitter = new Twitter("student", "password");
+					// twitter.setAPIRootUrl("http://yamba.markana.com/api");
+					twitter.setStatus(statusText);
+				} catch (TwitterException e) {
+					Log.e(TAG, "Died", e);
+					e.printStackTrace();
+				}
+			}
+		}.start();
+
 		Log.d(TAG, "onClicked with text: " + statusText);
 	}
 }
